8d26c3edf3c85004a8eb3233463bb5abcab59c61
Rajesh Menon <rajeshmenon@rennovationsoftware.com>
2022-05-19 13:05:01 +0530

    transactions select specific columns

--- app/Models/TransactionsModel.php
+++ app/Models/TransactionsModel.php
@@ -12,7 +12,7 @@ class TransactionsModel extends Model
 
     public function getTransaction($acnt_id, $filter, $pageNo, $pageSize)
     {
-        $result = $this->builder()->select("transactions.*, acnt_dr.acnt_name as acnt_name_dr, acnt_cr.acnt_name as acnt_name_cr, if(acnt_dr.acnt_id = '" . $acnt_id . "', 'DR', 'CR') as etype")
+        $result = $this->builder()->select("txn_id, txn_date, txn_ref_nr, txn_acnt_id_dr, txn_acnt_id_cr, txn_amount_dr as txn_amount, txn_remarks, acnt_dr.acnt_name as acnt_name_dr, acnt_cr.acnt_name as acnt_name_cr, if(acnt_dr.acnt_id = '" . $acnt_id . "', 'DR', 'CR') as etype")
             ->join('accounts acnt_dr', 'acnt_dr.acnt_id = transactions.txn_acnt_id_dr', 'inner')
             ->join('accounts acnt_cr', 'acnt_cr.acnt_id = transactions.txn_acnt_id_cr', 'inner')
             ->where("(txn_acnt_id_dr = '" . $acnt_id . "') OR (txn_acnt_id_cr = '" . $acnt_id . "') " . $filter)
@@ -23,7 +23,7 @@ class TransactionsModel extends Model
         return $result;
     }
     public function getTransactionsCount($acnt_id, $filter) {
-        $result = $this->builder()->select("transactions.*, acnt_dr.acnt_name as acnt_name_dr, acnt_cr.acnt_name as acnt_name_cr, if(acnt_dr.acnt_id = '" . $acnt_id . "', 'DR', 'CR') as etype")
+        $result = $this->builder()->select("transactions.*")
             ->join('accounts acnt_dr', 'acnt_dr.acnt_id = transactions.txn_acnt_id_dr', 'inner')
             ->join('accounts acnt_cr', 'acnt_cr.acnt_id = transactions.txn_acnt_id_cr', 'inner')
             ->where("(txn_acnt_id_dr = '" . $acnt_id . "') OR (txn_acnt_id_cr = '" . $acnt_id . "') " . $filter)
